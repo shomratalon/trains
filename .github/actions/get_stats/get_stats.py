1c0a416671b58b9618bdced93d8699b023e9a2b6
alonshomrat <14982269+shomratalon@users.noreply.github.com>
2020-07-19 18:35:14 +0300

    [Added] init workflow

--- .github/actions/get_stats/get_stats.py
+++ .github/actions/get_stats/get_stats.py
@@ -23,16 +23,18 @@ def get_project_stats():
 
 def create_stats_comment(project_stats):
     payload_fname = os.getenv('GITHUB_EVENT_PATH')
-    print(f"payload_fname: {payload_fname}")
+    # print(f"payload_fname: {payload_fname}")
     with open(payload_fname, 'r') as f:
         payload = json.load(f)
-    print(f"Payload: {payload}")
+    # print(f"Payload: {payload}")
     owner, repo = payload.get("repository", {}).get("full_name", "").split("/")
     if owner and repo:
+        print(f"GITHUB_TOKEN {os.getenv('secrets.GITHUB_TOKEN')}")
         gh = login(token=os.getenv("secrets.GITHUB_TOKEN"))
-        issue = gh.issue(owner, repo, payload.get("issue", {}).get("number"))
-        if issue:
-            issue.create_comment(project_stats)
+        if gh:
+            issue = gh.issue(owner, repo, payload.get("issue", {}).get("number"))
+            if issue:
+                issue.create_comment(project_stats)
 
 
 if __name__ == "__main__":
