78a9caac626c613f6a2b4cedd11d69cc381ead81
alonshomrat <14982269+shomratalon@users.noreply.github.com>
2020-07-20 16:12:06 +0300

    [Added] init workflow

---
+++ .github/actions/report_scalars/report_scalars.py
@@ -0,0 +1,59 @@
+"""
+Get the statistics of your task's in a project
+
+Need to define in project secrets the follow:
+ - TRAINS_API_ACCESS_KEY
+ - TRAINS_API_SECRET_KEY
+ - TRAINS_API_HOST
+ - PROJECT_NAME
+ - TASK_ID
+"""
+import json
+import os
+
+from tabulate import tabulate
+import pandas as pd
+from github3 import login
+
+from trains.backend_api.session.client import APIClient
+
+
+def get_project_scalars():
+    client = APIClient()
+    task_id = os.getenv('TASK_ID')
+    if task_id:
+        task = client.tasks.get_by_id(task=task_id)
+        last_metrics = task.last_metrics
+        data = []
+        columns = []
+        for _, metric_val in list(last_metrics.values())[0].items():
+            data.append([val for val in metric_val.values()])
+            if not columns:
+                columns = [key for key in metric_val.keys()]
+        df = pd.DataFrame(data=data, columns=columns)
+        table = tabulate(df, tablefmt="github", headers="keys", showindex=False)
+        return f"Task {task_id} Results\n\n{table}\n\n"
+    else:
+        return f"Please add `TASK_ID` to repo's secrets."
+
+
+def create_stats_comment(project_stats):
+    payload_fname = os.getenv('GITHUB_EVENT_PATH')
+    with open(payload_fname, 'r') as f:
+        payload = json.load(f)
+    owner, repo = payload.get("repository", {}).get("full_name", "").split("/")
+    if owner and repo:
+        gh = login(token=os.getenv("GITHUB_TOKEN"))
+        if gh:
+            issue = gh.issue(owner, repo, payload.get("issue", {}).get("number"))
+            if issue:
+                issue.create_comment(project_stats)
+            else:
+                print(f'can not comment issue, {payload.get("issue", {}).get("number")}')
+        else:
+            print(f"can not log in to gh, {os.getenv('GITHUB_TOKEN')}")
+
+
+if __name__ == "__main__":
+    stats = get_project_scalars()
+    create_stats_comment(stats)
