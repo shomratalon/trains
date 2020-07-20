e610a8f19af2d782845275a6a59e1165931d7a6b
alonshomrat <14982269+shomratalon@users.noreply.github.com>
2020-07-20 14:21:21 +0300

    [Added] init workflow

--- .github/actions/get_stats/get_stats.py
+++ .github/actions/get_stats/get_stats.py
@@ -10,21 +10,28 @@ from trains.backend_api.session.client import APIClient
 
 def get_project_stats():
     client = APIClient()
-    project = client.projects.get_all(name="examples")
-    projects_id = project._result.response_data['projects'][0]['id']
-    tasks = client.tasks.get_all(project=[projects_id])
-    stats_dict = {}
-    for task in tasks:
-        key = str(task.status)
-        if key in stats_dict:
-            stats_dict[key] += 1
+    project_name = os.getenv('PROJECT_NAME')
+    if project_name:
+        project = client.projects.get_all(name=project_name)
+        if project:
+            projects_id = project._result.response_data['projects'][0]['id']
+            tasks = client.tasks.get_all(project=[projects_id])
+            stats_dict = {}
+            for task in tasks:
+                key = str(task.status)
+                if key in stats_dict:
+                    stats_dict[key] += 1
+                else:
+                    stats_dict[key] = 1
+            df = pd.DataFrame(data=stats_dict.items(), columns=["Status", "Count"])
+            table = tabulate(df, tablefmt="github", headers="keys", showindex=False)
+            return f"Project {projects_id} Results\n\n{table}\n\n" \
+                   f"Results can be found [here]({project.service.session.host}/projects/{projects_id})"
         else:
-            stats_dict[key] = 1
-
-    df = pd.DataFrame(data=stats_dict.items(), columns=["Status", "Count"])
-    table = tabulate(df, tablefmt="github", headers="keys", showindex=False)
-    return f"Project {projects_id} Results\n\n{table}\n\n" \
-           f"Results can be found [here]({project.service.session.host}/projects/{projects_id})"
+            return f"Can not find any tasks for project {project_name}.\n\n" \
+                   f"You can view all your projects [here]({project.service.session.host}/projects)"
+    else:
+        return f"Please add `PROJECT_NAME` to repo's secrets."
 
 
 def create_stats_comment(project_stats):
